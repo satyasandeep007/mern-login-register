4d6d6c9cc265888ef17139ef02be05b46b48cd00
Sandeep <sandeepchowdarymallineni007@gmail.com>
2020-01-10 20:47:57 +0530

    deployed

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
 let routes = require('./routes')
 
@@ -20,6 +21,16 @@ connection.once('open', () => {
 connection.on('error', (e) => console.log("error"));
 
 app.use('/', routes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`); 
